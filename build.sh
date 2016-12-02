1fc37de36032c3b7f0a1bce4eb0e86274ad6b4c0
d.pranchuk <d.pranchuk@gmail.com>
2016-12-03 00:39:12 +0300

    go build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-curl -o ca-certificates.crt https://raw.githubusercontent.com/bagder/ca-bundle/master/ca-bundle.crt
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
+curl -o ca-certificates.crt https://curl.haxx.se/ca/cacert.pem
 docker build -t cortwave/slack-publisher:$1 .
 rm ca-certificates.crt
 rm main
